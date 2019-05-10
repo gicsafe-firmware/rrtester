b998328d958930524b6a86b8db73f2dd6b886088
lf <francuccilea@gmail.com>
2019-05-10 15:10:24 -0300

    Added generated files of SigMon state machine

--- src/SigMon/inc/SigMonActRequired.h
+++ src/SigMon/inc/SigMonActRequired.h
@@ -1,36 +1,38 @@
+/**
+ *  \file       SigMonActRequired.h
+ *  \brief
+ */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ */
+
 /* --------------------------------- Notes --------------------------------- */
 /* --------------------------------- Module -------------------------------- */
-#ifndef __SIGMON_H__
-#define __SIGMON_H__
+#ifndef __SIGMONACTREQUIRED_H__
+#define __SIGMONACTREQUIRED_H__
 
 /* ----------------------------- Include files ----------------------------- */
-#include "rkhsma.h"
+#include "rkhtype.h"
+#include "SigMonActAccess.h"
 
 /* ---------------------- External C language linkage ---------------------- */
 #ifdef __cplusplus
-extern "C" {
+	extern "C" {
 #endif
 
 /* --------------------------------- Macros -------------------------------- */
 /* -------------------------------- Constants ------------------------------ */
-/* ........................ Declares active object ......................... */
-RKH_SMA_DCLR(sigMon);
-
-/* ................... Declares states and pseudostates .................... */
-RKH_DCLR_BASIC_STATE SigMon_Inactive, SigMon_WaitSyncSeq, SigMon_Seq0, SigMon_Seq2, SigMon_Seq3, SigMon_Seq4, SigMon_Seq5, SigMon_Seq11, SigMon_OutOfSeq, SigMon_Seq1, SigMon_Seq10, SigMon_Seq8, SigMon_Seq7, SigMon_Seq6, SigMon_Seq9;
-RKH_DCLR_COMP_STATE SigMon_Active;
-
 /* ------------------------------- Data types ------------------------------ */
-/* ............................. Active object ............................. */
-typedef struct SigMon SigMon;
-struct SigMon
-{
-    RKH_SMA_T sma;      /* base structure */
-    rInt digIn;
-};
-
 /* -------------------------- External variables --------------------------- */
 /* -------------------------- Function prototypes -------------------------- */
+rInt DigIn_get(void);
+void selfDispatchDigIn(rInt digIn);
+
 /* -------------------- External C language linkage end -------------------- */
 #ifdef __cplusplus
 }
