1e8e1977999b84698a6ce4b889de43a95332f0e7
lf <francuccilea@gmail.com>
2019-05-17 10:49:22 -0300

    Added DigIn module

---
+++ src/DigIn/inc/DigIn.h
@@ -0,0 +1,64 @@
+/*
+ *  --------------------------------------------------------------------------
+ *
+ *                               GICSAFe-Firmware
+ *                               ----------------
+ *
+ *                      Copyright (C) 2019 CONICET-GICSAFe
+ *          All rights reserved. Protected by international copyright laws.
+ *
+ *  Contact information:
+ *  site: https://github.com/gicsafe-firmware
+ *  e-mail: <someone>@<somewhere>
+ *  ---------------------------------------------------------------------------
+ */
+
+/**
+ *  \file   DigIn.h
+ *  \brief  Digital Input module specification.
+ */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  LeFr  Leandro Francucci lf@vortexmakes.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* --------------------------------- Module -------------------------------- */
+#ifndef __DIGIN_H__
+#define __DIGIN_H__
+
+/* ----------------------------- Include files ----------------------------- */
+/* ---------------------- External C language linkage ---------------------- */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* --------------------------------- Macros -------------------------------- */
+/* -------------------------------- Constants ------------------------------ */
+/* ------------------------------- Data types ------------------------------ */
+typedef struct DigIn DigIn;
+struct DigIn
+{
+    unsigned clk : 1;
+    unsigned clkX3 : 1;
+    unsigned clkX6 : 1;
+};
+
+/* -------------------------- External variables --------------------------- */
+/* -------------------------- Function prototypes -------------------------- */
+DigIn DigIn_get(void);
+
+/* -------------------- External C language linkage end -------------------- */
+#ifdef __cplusplus
+}
+#endif
+
+/* ------------------------------ Module end ------------------------------- */
+#endif
+
+/* ------------------------------ End of file ------------------------------ */
