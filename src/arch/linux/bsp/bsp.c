a8d852b20efc35822f78ac4c77aa5f3ae271a433
Carlos Mancón <manconci@gmail.com>
2019-06-04 08:53:45 -0300

    Applied Uncrustify to bsp.c

--- src/arch/linux/bsp/bsp.c
+++ src/arch/linux/bsp/bsp.c
@@ -56,9 +56,9 @@ RKH_THIS_MODULE
 
 /* ---------------------------- Local data types --------------------------- */
 /* ---------------------------- Global variables --------------------------- */
-SERIAL_T serials[ NUM_CHANNELS ] =
+SERIAL_T serials[NUM_CHANNELS] =
 {
-	{	"/dev/ttyUSB1",	19200, 8, PAR_NONE, STOP_1},
+    {"/dev/ttyUSB1", 19200, 8, PAR_NONE, STOP_1},
 };
 static FILE *fGsmLog = NULL;
 
@@ -69,12 +69,12 @@ static char *opts = (char *)RRTESTER_CFG_OPTIONS;
 static const char *helpMessage =
 {
     "\nOption usage:\n"
-	"\t -s silence\n"
+    "\t -s silence\n"
     "\t -f File name for binary trace output\n"
     "\t -t ipaddr of TCP trace client\n"
     "\t -p port of TCP trace client\n"
-	"\t -b ipaddr of MQTT Broker\n"
-	"\t -q port of MQTT Broker\n"
+    "\t -b ipaddr of MQTT Broker\n"
+    "\t -q port of MQTT Broker\n"
     "\t -m GSM Module Serial Port\n"
     "\t -h (help)\n"
 };
@@ -92,7 +92,7 @@ static SendEvt e_publishTout;
 static void ser_rx_isr(unsigned char byte);
 static void ser_tx_isr(void);
 static SERIAL_CBACK_T ser_cback =
-{ ser_rx_isr, NULL, NULL, ser_tx_isr, NULL, NULL, NULL };
+{ser_rx_isr, NULL, NULL, ser_tx_isr, NULL, NULL, NULL};
 
 /* ----------------------- Local function prototypes ----------------------- */
 /* ---------------------------- Local functions ---------------------------- */
@@ -103,8 +103,8 @@ printBanner(void)
     printf("RKH version      = %s\n", RKH_RELEASE);
     printf("Port version     = %s\n", rkhport_get_version());
     printf("Port description = %s\n\n", rkhport_get_desc());
-	printf("Description: \n\n"
-		"Sistema probador de relés ferroviarios\n");
+    printf("Description: \n\n"
+           "Sistema probador de relés ferroviarios\n");
 
     printf("1.- Press ESC to quit \n\n\n");
 }
@@ -121,9 +121,9 @@ processCmdLineOpts(int argc, char **argv)
                 strcpy(serials[GSM_PORT].com_name, optarg);
                 break;
 
-			case 's':
-				trace_io_silence();
-				break;
+            case 's':
+                trace_io_silence();
+                break;
 
             case 'f':
                 trace_io_setFileName(optarg);
@@ -138,12 +138,12 @@ processCmdLineOpts(int argc, char **argv)
                 break;
 
             case 'b':
-            	config_brokerAddress(optarg);
-            	break;
+                config_brokerAddress(optarg);
+                break;
 
             case 'q':
-            	config_brokerPort(optarg);
-            	break;
+                config_brokerPort(optarg);
+                break;
 
             case '?':
             case 'h':
@@ -162,33 +162,33 @@ send_signalsFrame(void)
     char *p;
 
     RKH_SET_STATIC_EVENT(RKH_UPCAST(RKH_EVT_T, &e_Send), evSend);
-   
+
     n = anSampler_getSet(&anSet, NUM_AN_SAMPLES_GET);
-   
+
     p = (char *)(e_Send.buf);
     l = 0;
 
-    for(i=0; i < NUM_AN_SIGNALS; ++i)
+    for (i=0; i < NUM_AN_SIGNALS; ++i)
     {
         l += sprintf(p + l, "ts:%u, AN[%d]", anSet.timeStamp, i);
 
-        for(j=0; j<n; ++j)
+        for (j=0; j < n; ++j)
         {
             l += sprintf(p + l,", %d.%02d",
-                               (char)((anSet.anSignal[i][j] & 0xFF00) >> 8),
-                               (char)(anSet.anSignal[i][j] & 0x00FF));
+                         (char)((anSet.anSignal[i][j] & 0xFF00) >> 8),
+                         (char)(anSet.anSignal[i][j] & 0x00FF));
         }
 
         l += sprintf(p + l, "\r\n");
     }
 
     n = IOChgDet_get(ioChg, NUM_DI_SAMPLES_GET);
-    
-    for(i=0; i < n; ++i)
+
+    for (i=0; i < n; ++i)
     {
         l += sprintf(p + l, "ts:%u, DI[%d]:%d\r\n", ioChg[i].timeStamp,
-                                                    ioChg[i].signalId,
-                                                    ioChg[i].signalValue );
+                     ioChg[i].signalId,
+                     ioChg[i].signalValue);
     }
 
     e_Send.size = l;
@@ -212,7 +212,7 @@ bsp_init(int argc, char *argv[])
 
     modPwr_init();
     dIn_init();
-	anIn_init();
+    anIn_init();
 #ifdef USE_ETH
     eth_init();
 #endif
@@ -222,11 +222,11 @@ void
 closeCommInterface()
 {
 #ifdef USE_GSM
-	printf("Close GPRS Socket\r\n");
-	ConnectionTopic_publish(&e_Close, &bsp);
+    printf("Close GPRS Socket\r\n");
+    ConnectionTopic_publish(&e_Close, &bsp);
 #else
-	printf("Disconnect ConMgrEth\r\n");
-	ConnectionTopic_publish(&e_disconnected, &bsp);
+    printf("Disconnect ConMgrEth\r\n");
+    ConnectionTopic_publish(&e_disconnected, &bsp);
 #endif
 }
 
@@ -234,36 +234,38 @@ void
 openCommInterface()
 {
 #ifdef USE_GSM
-	printf("Open GPRS Socket\r\n");
-	ConnectionTopic_publish(&e_Open, &bsp);
+    printf("Open GPRS Socket\r\n");
+    ConnectionTopic_publish(&e_Open, &bsp);
 #else
-	printf("Connect ConMgrEth\r\n");
-	ConnectionTopic_publish(&e_connected, &bsp);
+    printf("Connect ConMgrEth\r\n");
+    ConnectionTopic_publish(&e_connected, &bsp);
 #endif
 }
 
-void forcePublication() {
-	printf("Force Publication.\r\n");
-	RKH_SET_STATIC_EVENT(RKH_UPCAST(RKH_EVT_T, &e_publishTout),
-			evWaitPublishTout);
-	RKH_SMA_POST_FIFO(mqttProt, &e_publishTout, &bsp);
+void
+forcePublication()
+{
+    printf("Force Publication.\r\n");
+    RKH_SET_STATIC_EVENT(RKH_UPCAST(RKH_EVT_T, &e_publishTout),
+                         evWaitPublishTout);
+    RKH_SMA_POST_FIFO(mqttProt, &e_publishTout, &bsp);
 }
 
 void
 bsp_keyParser(int c)
 {
-    switch(c)
+    switch (c)
     {
         case ESC:
             rkhport_fwk_stop();
             break;
 
         case 'o':
-        	openCommInterface();
+            openCommInterface();
             break;
 
         case 'c':
-        	closeCommInterface();
+            closeCommInterface();
             break;
 
         case 'r':
@@ -283,25 +285,27 @@ bsp_keyParser(int c)
             break;
 
         case 'f':
-        	forcePublication();
+            forcePublication();
             break;
+
         case 'q':
-        	forcePublication();
-        	Sleep(4000);	// Wait until publication is made
-        	printf("Disconnect.\r\n");
-        	eth_socketClose();
+            forcePublication();
+            Sleep(4000);    /* Wait until publication is made */
+            printf("Disconnect.\r\n");
+            eth_socketClose();
             break;
+
         case 'a':
             send_signalsFrame();
             break;
 
-		case 'j':
-			modPwr_off();
-			break;
+        case 'j':
+            modPwr_off();
+            break;
 
-		case 'k':
-			modPwr_on();
-			break;
+        case 'k':
+            modPwr_on();
+            break;
 
         default:
             break;
@@ -317,19 +321,19 @@ bsp_timeTick(void)
 
 static
 void
-ser_rx_isr( unsigned char byte )
+ser_rx_isr(unsigned char byte)
 {
     cmdParser(byte);
     if (fGsmLog != NULL)
     {
         fwrite(&byte, 1, 1, fGsmLog);
-		fflush(fGsmLog);
+        fflush(fGsmLog);
     }
 }
 
 static
 void
-ser_tx_isr( void )
+ser_tx_isr(void)
 {
 }
 
@@ -341,36 +345,38 @@ bsp_eth_open(void)
 void
 bsp_serial_open(int ch)
 {
-	if ((fGsmLog = fopen("UartGSM.log","w+b")) == NULL)
-		printf("Can't open uart log file for GSM\n");
+    if ((fGsmLog = fopen("UartGSM.log","w+b")) == NULL)
+    {
+        printf("Can't open uart log file for GSM\n");
+    }
 
     cmdParser = ModCmd_init();
-    init_serial_hard(ch, &ser_cback );
+    init_serial_hard(ch, &ser_cback);
     connect_serial(ch);
-	set_dtr(ch);
-	Sleep(500);
+    set_dtr(ch);
+    Sleep(500);
     RKH_TR_FWK_ACTOR(&bsp, "bsp");
 }
 
 void
 bsp_serial_close(int ch)
 {
-	set_dtr(ch);
-	disconnect_serial(ch);
-	deinit_serial_hard(ch);
-	fclose(fGsmLog);
+    set_dtr(ch);
+    disconnect_serial(ch);
+    deinit_serial_hard(ch);
+    fclose(fGsmLog);
 }
 
 void
 bsp_serial_puts(int ch, char *p)
 {
-    while(*p!='\0')
+    while (*p != '\0')
     {
-		if (fGsmLog != NULL)
-		{
-			fwrite(p, 1, 1, fGsmLog);
-			fflush(fGsmLog);
-		}
+        if (fGsmLog != NULL)
+        {
+            fwrite(p, 1, 1, fGsmLog);
+            fflush(fGsmLog);
+        }
         tx_data(ch, *p);
         ++p;
     }
@@ -379,7 +385,7 @@ bsp_serial_puts(int ch, char *p)
 void
 bsp_serial_putnchar(int ch, unsigned char *p, ruint ndata)
 {
-    while(ndata && (ndata-- != 0))
+    while (ndata && (ndata-- != 0))
     {
         tx_data(ch, *p);
         ++p;
@@ -389,27 +395,27 @@ bsp_serial_putnchar(int ch, unsigned char *p, ruint ndata)
 void
 bsp_linkStatus(NetType_t t, Status_t status)
 {
-    if(t == GSMNetwork)
+    if (t == GSMNetwork)
     {
-        printf("\r\nGSM Network %s\r\n", 
-            status == RegisteredSt ? "Registered" : "Unregistered");
+        printf("\r\nGSM Network %s\r\n",
+               status == RegisteredSt ? "Registered" : "Unregistered");
     }
     else
     {
-        printf("\r\nETH Link %s\r\n", 
-            status == ConnectedSt ? "Connected" : "Disconnected");
+        printf("\r\nETH Link %s\r\n",
+               status == ConnectedSt ? "Connected" : "Disconnected");
     }
 }
 
-void 
+void
 bsp_socketStatus(NetType_t t, Status_t status)
 {
-    printf("\r\n%s Socket %s\r\n", 
-            t == GSMNetwork ? "GSM" : "ETH",
-            status == ConnectedSt ? "Connected" : "Disconnected");
+    printf("\r\n%s Socket %s\r\n",
+           t == GSMNetwork ? "GSM" : "ETH",
+           status == ConnectedSt ? "Connected" : "Disconnected");
 }
 
-void 
+void
 bsp_modStatusToggle(void)
 {
 }
